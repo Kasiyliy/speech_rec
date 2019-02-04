76af7651da53b41bbc28b69f13e70e741c833ee1
omar <dinasil.omarbek@gmail.com>
2019-02-04 17:28:42 +0600

    added inserting questions

--- src/main/java/kz/halyq/speech_rec/rest/controllers/MainController.java
+++ src/main/java/kz/halyq/speech_rec/rest/controllers/MainController.java
@@ -1,14 +1,17 @@
 package kz.halyq.speech_rec.rest.controllers;
 
 import kz.halyq.speech_rec.services.RecognizerService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.activation.MimetypesFileTypeMap;
-import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 /**
  * @author Assylkhan
@@ -26,16 +29,42 @@ public class MainController {
     @PostMapping("/uploadFile")
     public String uploadFile(@RequestParam("file") MultipartFile file) {
         String files = file.getContentType();
-        if(!files.equals(MIME_TYPE)){
-            return  "{ \"message\" : \"mime type error\" , \"error\" : true  }";
+        if (!files.equals(MIME_TYPE)) {
+            return "{ \"message\" : \"mime type error\" , \"error\" : true  }";
         }
         String result = "";
-        try{
+        try {
             result += recognizerService.recognize(file);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return  result;
+        return result;
+    }
+
+    @PostMapping("set_questions")
+    public String setQuestions(String data) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/speech",
+                    "root", "sbeezzs02");
+
+            JSONObject jsonData = new JSONObject(data);
+            String code = jsonData.get("code").toString();
+            String answer = jsonData.get("answer").toString();
+            int points = Integer.parseInt(jsonData.get("points").toString());
+
+            String sql = "insert into questions (code, answer, points)\n" +
+                    "values (?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, code);
+            preparedStatement.setString(2, answer);
+            preparedStatement.setInt(3, points);
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "{ \"message\" : \"successful\" , \"error\" : false  }";
     }
 
 }
